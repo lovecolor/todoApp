21d7adb7b835b81f036cfe57e27d233f5ec855fa
Your Name <hoanglong0507000@gmail.co$ git config --global user.email hoanglong0507000@gmail.co>
2021-07-14 16:42:24 +0700

    update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -31,30 +31,11 @@ function Copyright() {
   )
 }
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}))
 export const Login = () => {
   // to call API
   // const api = useAppApiClient()
   // api.login({ username: "abc", password: "def" })
-  const classes = useStyles()
+
   const authCtx = useContext(AuthContext)
   const submitHandler = (e) => {
     e.preventDefault()
